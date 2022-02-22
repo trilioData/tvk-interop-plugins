9f2f6664e9d31511959cee09987785826041e72d
root <root@linuxinstance2.c.tvk-solutions-330321.internal>
2022-02-22 08:30:16 +0000

    check-git-diff-between-tags.sh

--- hack/check-git-diff-between-tags.sh
+++ hack/check-git-diff-between-tags.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+set -x
 # shellcheck disable=SC2046
 # shellcheck disable=SC2006
 current_tag=$(git describe --abbrev=0 --tags)
@@ -12,8 +13,9 @@ git describe --exact-match --tags --match "$current_tag"
 # shellcheck disable=SC2046
 # shellcheck disable=SC2006
 # add flag '-v -e "v*-rc*"', if need to compute diff from last stable tag instead of RC
-previous_tag=$(git tag --sort=-creatordate | grep -e "v[0-9].[0-9].[0-9]" | grep -v -e "$current_tag" -e "v*-alpha*" -e "v*-beta*" | head -n 1)
+#previous_tag=$(git tag --sort=-creatordate | grep -e "v[0-9].[0-9].[0-9]" | grep -v -e "$current_tag" -e "v*-alpha*" -e "v*-beta*" | head -n 1)
 # use hard coded values if required
+previous_tag="v0.0.0"
 #current_tag=v0.0.6-main
 #previous_tag=v0.0.5-dev
 
@@ -29,9 +31,12 @@ tools_dir="tools"
 tvk_oneclick_dir=$tools_dir/tvk-oneclick
 
 # shellcheck disable=SC2086
-git diff --name-only $previous_tag $current_tag $tools_dir >files.txt
+#git diff --name-only $previous_tag $current_tag $tools_dir >files.txt
 # shellcheck disable=SC2086
-git diff --name-only $previous_tag $current_tag $cmd_dir >>files.txt
+#git diff --name-only $previous_tag $current_tag $cmd_dir >>files.txt
+
+echo "As this is first release v1.0.0" >files.txt
+echo "Skipping previous and new tags comparison" >>files.txt
 
 count=$(wc -l <files.txt)
 if [[ $count -eq 0 ]]; then
@@ -51,6 +56,7 @@ while IFS= read -r file; do
   fi
 
 done <files.txt
+tvk_oneclick_changed=true # only for first release
 
 if [[ $tvk_oneclick_changed == true ]]; then
   echo "Creating Release as files related to tvk-oneclick"
