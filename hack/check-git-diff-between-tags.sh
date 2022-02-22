248d6a97607d871b4840ab161c21fcfc34301c11
root <root@linuxinstance2.c.tvk-solutions-330321.internal>
2022-02-22 07:09:07 +0000

    changed check-git-diff-between-tags

--- hack/check-git-diff-between-tags.sh
+++ hack/check-git-diff-between-tags.sh
@@ -21,21 +21,12 @@ echo "current_tag=$current_tag and previous_tag=$previous_tag"
 
 echo "checking paths of modified files-"
 
-preflight_changed=false
-log_collector_changed=false
-target_browser_changed=false
 tvk_oneclick_changed=false
-cleanup_changed=false
 
 cmd_dir="cmd"
 tools_dir="tools"
-log_collector_dir="log-collector"
-internal_dir="internal"
-target_browser_dir="target-browser"
-preflight_dir="preflight"
 
 tvk_oneclick_dir=$tools_dir/tvk-oneclick
-cleanup_dir=$tools_dir/cleanup
 
 # shellcheck disable=SC2086
 git diff --name-only $previous_tag $current_tag $tools_dir >files.txt
@@ -55,40 +46,16 @@ echo "list of modified files-"
 cat files.txt
 
 while IFS= read -r file; do
-  if [[ ($preflight_changed == false) && ($file == $internal_dir/* || $file == $tools_dir/$preflight_dir/* || $file == $cmd_dir/$preflight_dir/*) ]]; then
-    echo "preflight related code changes have been detected"
-    echo "::set-output name=release_preflight::true"
-    preflight_changed=true
-  fi
-
-  if [[ ($log_collector_changed == false) && ($file == $internal_dir/* || $file == $tools_dir/$log_collector_dir/* || $file == $cmd_dir/$log_collector_dir/*) ]]; then
-    echo "log-collector related code changes have been detected"
-    echo "::set-output name=release_log_collector::true"
-    log_collector_changed=true
-  fi
-
-  if [[ ($target_browser_changed == false) && ($file == $internal_dir/* || $file == $tools_dir/$target_browser_dir/* || $file == $cmd_dir/$target_browser_dir/*) ]]; then
-    echo "target-browser related code changes have been detected"
-    echo "::set-output name=release_target_browser::true"
-    target_browser_changed=true
-  fi
-
   if [[ $tvk_oneclick_changed == false && $file == $tvk_oneclick_dir/* ]]; then
     echo "tvk-oneclick related code changes have been detected"
     echo "::set-output name=release_tvk_oneclick::true"
     tvk_oneclick_changed=true
   fi
 
-  if [[ $cleanup_changed == false && $file == $cleanup_dir/* ]]; then
-    echo "cleanup related code changes have been detected"
-    echo "::set-output name=release_cleanup::true"
-    cleanup_changed=true
-  fi
-
 done <files.txt
 
-if [[ $preflight_changed == true || $log_collector_changed == true || $target_browser_changed == true || $tvk_oneclick_changed == true || $cleanup_changed ]]; then
-  echo "Creating Release as files related to preflight, log-collector, target-browser, tvk-oneclick or cleanup have been changed"
+if [[ $tvk_oneclick_changed == true ]]; then
+  echo "Creating Release as files related to tvk-oneclick"
   echo "::set-output name=create_release::true"
 fi
 
@@ -98,5 +65,5 @@ fi
 #echo "::set-output name=release_preflight::true"
 #echo "::set-output name=release_log_collector::true"
 #echo "::set-output name=release_target_browser::true"
-#echo "::set-output name=release_tvk_oneclick::true"
+echo "::set-output name=release_tvk_oneclick::true"
 #echo "::set-output name=release_cleanup::true"
